af3c21bc481ea944e31891a9bdf4e3d26ce5022b
Ruby Jha <rubyjha@gmail.com>
2023-05-16 10:32:31 -0400

    Added implementation for expanding panels

--- 50projects-50days/day1/expanding-cards/script.js
+++ 50projects-50days/day1/expanding-cards/script.js
@@ -1,1 +1,13 @@
 
+const panels = document.querySelectorAll('.panel');
+panels.forEach(panel => {
+    panel.addEventListener('click' , () =>{
+        console.log('click event called');
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
+});
+
+function removeActiveClasses(){
+    panels.forEach(panel => panel.classList.remove('active'));
+}
